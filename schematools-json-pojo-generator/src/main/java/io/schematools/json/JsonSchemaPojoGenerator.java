cd7c97ba9beb2148440513a3f9ef812ce018e912
Stephen Nimmo <stephennimmo@gmail.com>
2023-06-09 05:51:53 -0500

    simple example works

--- schematools-json-pojo-generator/src/main/java/io/schematools/json/JsonSchemaPojoGenerator.java
+++ schematools-json-pojo-generator/src/main/java/io/schematools/json/JsonSchemaPojoGenerator.java
@@ -1,18 +1,28 @@
 package io.schematools.json;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.expr.StringLiteralExpr;
+import jakarta.annotation.Generated;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class JsonSchemaPojoGenerator {
@@ -44,7 +54,15 @@ public class JsonSchemaPojoGenerator {
     private void generate(JsonSchema jsonSchema) {
         CompilationUnit compilationUnit = new CompilationUnit();
         compilationUnit.setPackageDeclaration(jsonSchema.idAdapter().packageName());
-        compilationUnit.addClass(jsonSchema.getClassName());
+        ClassOrInterfaceDeclaration classOrInterfaceDeclaration = compilationUnit.addClass(jsonSchema.getClassName());
+        classOrInterfaceDeclaration.addAndGetAnnotation(Generated.class)
+                .addPair("value", new StringLiteralExpr("io.schematools"));
+        for(Map.Entry<String, JsonNode> entry : jsonSchema.jsonNode().get("properties").properties()) {
+            handleProperty(entry.getKey(), entry.getValue(), classOrInterfaceDeclaration);
+        }
+        for (final JsonNode node : jsonSchema.jsonNode().get("required")){
+            this.handleRequired(node, classOrInterfaceDeclaration);
+        }
 
         try {
             String outputPath = jsonSchemaPojoGeneratorConfiguration.targetPath() + "/" + jsonSchema.idAdapter().packageName().replace('.', '/');
@@ -56,6 +74,40 @@ public class JsonSchemaPojoGenerator {
             System.out.println("Java source file generated successfully: " + file.getAbsolutePath());
         } catch (IOException e) {
             System.out.println("An error occurred while generating the Java source file: " + e.getMessage());
+        }
+    }
+
+    private void handleProperty(String name, JsonNode current, ClassOrInterfaceDeclaration classOrInterfaceDeclaration) {
+        String type = current.get("type").asText();
+        Class<?> clazz = getClassForType(type);
+        FieldDeclaration fieldDeclaration = classOrInterfaceDeclaration.addField(clazz, CaseHelper.convertToCamelCase(name, false), Modifier.Keyword.PUBLIC);
+        fieldDeclaration.addAndGetAnnotation(JsonProperty.class)
+                .addPair("value", new StringLiteralExpr(name));
+    }
+
+    private void handleRequired(JsonNode jsonNode, ClassOrInterfaceDeclaration classOrInterfaceDeclaration) {
+        FieldDeclaration fieldDeclaration = classOrInterfaceDeclaration.getFieldByName(CaseHelper.convertToCamelCase(jsonNode.asText(), false)).get();
+        String type = fieldDeclaration.getVariable(0).getType().toString();
+        fieldDeclaration.addAndGetAnnotation(getAnnotationForType(type));
+    }
+
+    public Class<?> getClassForType(String type) {
+        switch (type) {
+            case "string" -> {
+                return String.class;
+            }
+            default -> throw new RuntimeException("Unknown Class for node type: " + type);
+        }
+    }
+
+    public Class<? extends Annotation> getAnnotationForType(String type) {
+        switch (type) {
+            case "String" -> {
+                return NotEmpty.class;
+            }
+            default -> {
+                return NotNull.class;
+            }
         }
     }
 
--- schematools-json-pojo-generator/src/test/java/io/schematools/json/JsonSchemaPojoGeneratorTest.java
+++ schematools-json-pojo-generator/src/test/java/io/schematools/json/JsonSchemaPojoGeneratorTest.java
@@ -2,12 +2,9 @@ package io.schematools.json;
 
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.expr.*;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.stmt.ExpressionStmt;
-import com.github.javaparser.ast.stmt.ReturnStmt;
+import com.github.javaparser.ast.expr.Name;
+import jakarta.annotation.Generated;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -19,12 +16,19 @@ public class JsonSchemaPojoGeneratorTest {
 
     @Test
     void simple() throws IOException {
+        String expectedFilePath = "target/generated-sources/com/example/schemas/AddressV1.java";
         JsonSchemaPojoGenerator jsonSchemaPojoGenerator = new JsonSchemaPojoGenerator(new JsonSchemaPojoGeneratorConfiguration("src/test/resources/schema/simple", "target/generated-sources"));
         jsonSchemaPojoGenerator.generate();
-        assertThat(new File("target/generated-sources/com/example/schemas/AddressV1.java")).exists();
-        CompilationUnit compilationUnit = StaticJavaParser.parse(new File("target/generated-sources/com/example/schemas/AddressV1.java"));
+        assertThat(new File(expectedFilePath)).exists();
+        CompilationUnit compilationUnit = StaticJavaParser.parse(new File(expectedFilePath));
         Name name = compilationUnit.getPackageDeclaration().get().getName();
         assertThat(name.toString()).isEqualTo("com.example.schemas");
+        ClassOrInterfaceDeclaration classOrInterfaceDeclaration = compilationUnit.getClassByName("AddressV1").get();
+        assertThat(classOrInterfaceDeclaration.getAnnotationByClass(Generated.class)).isNotEmpty();
+        assertThat(classOrInterfaceDeclaration.getFieldByName("streetAddress")).isNotEmpty();
+        assertThat(classOrInterfaceDeclaration.getFieldByName("city")).isNotEmpty();
+        assertThat(classOrInterfaceDeclaration.getFieldByName("state")).isNotEmpty();
+        assertThat(classOrInterfaceDeclaration.getFieldByName("zipCode")).isNotEmpty();
     }
 
 }
